93eb29fdf1936253517a5f23306e258081a2e0e8
Fausto Ismael Valdiviezo Gutierrez <fayo@MBP-de-Fausto.lan>
2018-01-17 19:40:58 -0600

    segundo comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { Pagina1Page } from '../pages/pagina1/pagina1';
 import { TabsPage } from '../pages/tabs/tabs';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    Pagina1Page,
     TabsPage
+  
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    Pagina1Page,
     TabsPage
+   
   ],
   providers: [
     StatusBar,
---
+++ src/pages/pagina1/pagina1.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'pagina1-contact',
+  templateUrl: 'pagina1.html'
+})
+export class Pagina1Page {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
+
